23f6d71ae13031d936cb702a1fd53a4bf1206e94
DanielRodriguez01 <danielrodriguezvexenat@outlook.com>
2025-09-09 16:47:45 -0300

    Materiales

---
+++ Backend.Models/Modelos/StockMateriales.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backend.BD.Modelos
+{
+    public class StockMateriales
+    {
+        [Key]
+        public int Id { get; set; }
+        public int Cantidad { get; set; }
+
+        [ForeignKey("MaterialesId")]
+        public int MaterialesId { get; set; }
+        public Materiales Material { get; set; }
+
+    }
+}
