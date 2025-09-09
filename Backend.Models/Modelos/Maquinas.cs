c8bc03251d95a18e194ec54c403629ef8bde536b
master16mh <master16.mh@gmail.com>
2025-09-09 16:42:43 -0300

    maquinas.ubi

---
+++ Backend.Models/Modelos/Maquinas.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backend.BD.Modelos
+{
+    public class Maquinas
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Marca { get; set; }
+        public string Modelo { get; set; }
+    }
+}
