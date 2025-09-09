c9cbe894528cbf249b6aceda0b69094f774f181a
MatiasAbregu <matiasabregu506@gmail.com>
2025-09-09 17:14:25 -0300

    fixing

--- Repositorios/obj/Debug/net8.0/Backend.Repositorios.AssemblyInfo.cs
+++ Repositorios/obj/Debug/net8.0/Backend.Repositorios.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend.Repositorios")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7705208b803db6f4084b84ad8ba375cb8bd7dda")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+16e1a3171e8abfc018e0d7403455238270d3cee9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend.Repositorios")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend.Repositorios")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
