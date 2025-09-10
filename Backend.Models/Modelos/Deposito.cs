30e0be2189ad7e6728202b4e259f174c30f60df0
master16mh <master16.mh@gmail.com>
2025-09-10 19:30:15 -0300

    UBICACION OBLIGATORIA

--- Backend.Models/Modelos/Deposito.cs
+++ Backend.Models/Modelos/Deposito.cs
@@ -11,12 +11,16 @@ namespace Backend.BD.Modelos
     {
         [Key]
         public int Id { get; set; }
+
         [Required(ErrorMessage = "El tipo del deposito es obligatorio.")]
-        public string? TipoDeposito { get; set; }
+        public string TipoDeposito { get; set; } = null!;
+
         [Required(ErrorMessage = "La obra del deposito es obligatorio.")]
         public int ObraId { get; set; }
-        public Obra Obra { get; set; }
+        public Obra Obra { get; set; } = null!;
+
+        [Required(ErrorMessage = "La ubicacion del deposito es obligatorio.")]
         public int UbicacionId { get; set; }
-        public Ubicacion Ubicacion { get; set; }
+        public Ubicacion Ubicacion { get; set; } = null!;
     }
 }
--- Backend.Models/Modelos/Ubicacion.cs
+++ Backend.Models/Modelos/Ubicacion.cs
@@ -11,10 +11,14 @@ namespace Backend.BD.Modelos
     {
         [Key]
         public int Id { get; set; }
-        public string CodigoUbicacion { get; set; }
-        public string Domicilio { get; set; }
-        public int ProvinciaId { get; set; }
-        public Provincia Provincia { get; set; }
 
+        [Required(ErrorMessage = "El codigo de ubicacion es obligatorio.")]
+        public string CodigoUbicacion { get; set; } = null!;
+
+        [Required(ErrorMessage = "El domicilio es obligatorio.")]
+        public string Domicilio { get; set; } = null!;
+
+        public int ProvinciaId { get; set; }
+        public Provincia Provincia { get; set; } = null!;
     }
 }
