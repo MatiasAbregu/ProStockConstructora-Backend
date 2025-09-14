394b955955f1f740bd0ccf6d6a8b1c4e7a940111
MatiasAbregu <matiasabregu506@gmail.com>
2025-09-14 02:28:39 -0300

    Me cansé de los merge

---
+++ Backend.Models/Modelos/NotaDePedido.cs
@@ -0,0 +1,32 @@
+﻿using Backend.BD.Enums;
+using Backend.BD.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backend.BD.Modelos
+{
+    [Index(nameof(NumeroNotaPedido), IsUnique = true)]
+    public class NotaDePedido
+    {
+        [Key]
+        public int Id { get; set; }
+        public required string NumeroNotaPedido { get; set; }
+
+        public required int DepositoDestinoId { get; set; }
+        public Deposito DepositoDestino { get; set; }
+
+        public required DateTime FechaEmision { get; set; }
+        public EstadoNotaPedido Estado { get; set; } = EstadoNotaPedido.Pendiente;
+
+        public required string SolicitadoPor {  get; set; }
+        public Usuario Usuario { get; set; }
+
+        // HACER EN DTO
+        //public List<DetalleNotaDePedido>? ListaDePedido { get; }
+    }
+}
