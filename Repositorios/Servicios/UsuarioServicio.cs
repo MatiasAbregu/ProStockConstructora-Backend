ee061bb64e2cd0c4f3be94d5a43f3e311d566e92
MatiasAbregu <matiasabregu506@gmail.com>
2025-09-15 16:41:45 -0300

    Controlador usuario

--- Repositorios/Servicios/UsuarioServicio.cs
+++ Repositorios/Servicios/UsuarioServicio.cs
@@ -105,17 +105,28 @@ namespace Backend.Repositorios.Servicios
 
         public async Task<IdentityResult> CrearUsuario(CrearUsuarioDTO usuario)
         {
+            using var transaction = await baseDeDatos.Database.BeginTransactionAsync();
+
             Usuario UsuarioBD = new(usuario.NombreUsuario, usuario.EmpresaId, usuario.Email, usuario.Celular);
             IdentityResult resultado = await gestorUsuarios.CreateAsync(UsuarioBD, usuario.Contrasena);
 
-            if (resultado.Succeeded) resultado = await gestorUsuarios.AddToRolesAsync(UsuarioBD, usuario.Roles);
+            if (resultado.Succeeded)
+            {
+                resultado = await gestorUsuarios.AddToRolesAsync(UsuarioBD, usuario.Roles);
+                if (!resultado.Succeeded)
+                {
+                    await transaction.RollbackAsync();
+                }
+            }
             else
             {
                 foreach (IdentityError error in resultado.Errors)
                 {
                     Debug.WriteLine(error.Description);
                 }
+                await transaction.RollbackAsync();
             }
+            await transaction.CommitAsync();
             return resultado;
         }
 
@@ -123,23 +134,67 @@ namespace Backend.Repositorios.Servicios
         {
             Usuario? usuarioBBDD = await gestorUsuarios.FindByIdAsync(id);
 
+            using var transaction = await baseDeDatos.Database.BeginTransactionAsync();
+
             if (usuarioBBDD == null) return (false, "El usuario que se desea actualizar no existe", null);
 
-            if(!string.IsNullOrEmpty(usuario.NombreUsuario)) 
+            if (!string.IsNullOrEmpty(usuario.NombreUsuario))
                 usuarioBBDD.UserName = usuario.NombreUsuario;
 
-            if(!string.IsNullOrEmpty(usuario.Celular)) 
+            if (!string.IsNullOrEmpty(usuario.Celular))
                 usuarioBBDD.PhoneNumber = usuario.Celular;
 
-            if(!string.IsNullOrEmpty(usuario.Email))
+            if (!string.IsNullOrEmpty(usuario.Email))
                 usuarioBBDD.Email = usuario.Email;
 
-            throw new NotImplementedException();
+            IdentityResult res;
+
+            if (usuario.Roles.Count > 0)
+            {
+                var rolesActuales = await gestorUsuarios.GetRolesAsync(usuarioBBDD);
+                var rolesAEliminar = rolesActuales.Except(usuario.Roles).ToList();
+                var rolesAAnadir = usuario.Roles.Except(rolesActuales).ToList();
+
+                if (rolesAEliminar.Count > 0)
+                {
+                    res = await gestorUsuarios.RemoveFromRolesAsync(usuarioBBDD, rolesAEliminar);
+                    if (!res.Succeeded)
+                    {
+                        await transaction.RollbackAsync();
+                        return (false, "Error al eliminar roles", null);
+                    }
+                }
+
+                if (rolesAAnadir.Count > 0)
+                {
+                    res = await gestorUsuarios.AddToRolesAsync(usuarioBBDD, rolesAAnadir);
+                    if (!res.Succeeded)
+                    {
+                        await transaction.RollbackAsync();
+                        return (false, "Error al añadir roles", null);
+                    }
+                }
+            }
+
+            res = await gestorUsuarios.UpdateAsync(usuarioBBDD);
+            if (!res.Succeeded) await transaction.RollbackAsync();
+
+            await transaction.CommitAsync();
+            return (true, "Usuario actualizado con éxito", usuarioBBDD);
         }
 
-        public Task<string> DesactivarUsuario()
+        public async Task<(bool, string)> DesactivarUsuario(string id)
         {
-            throw new NotImplementedException();
+            Usuario? usuarioBBDD = await gestorUsuarios.FindByIdAsync(id);
+
+            if (usuarioBBDD == null) return (false, "El usuario que se desea desactivar no existe");
+
+            usuarioBBDD.Estado = false;
+
+            var res = await gestorUsuarios.UpdateAsync(usuarioBBDD);
+
+            if (!res.Succeeded) return (false, "Ocurrio un error al desactivar el usuario");
+            return (true, "Usuario desactivado con éxito");
         }
     }
 }
