a86690fef1fe415dca760e92f7eda9abb2fce2f0
Marianela Aguirre <maryaguirre297@gmail.com>
2025-09-16 14:02:28 -0300

    Controller depositos

--- Backend/Controllers/ControladorDeposito.cs
+++ Backend/Controllers/ControladorDeposito.cs
@@ -1,13 +1,64 @@
-﻿using Backend.BD.Modelos;
+﻿using Backend.BD;
+using Backend.BD.Modelos;
+using Backend.DTO.DTOs_Depositos;
 using Backend.Repositorios.Implementaciones;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
 
 
 namespace Backend.Controllers
 {
-    [Route("api/deposito")]
     [ApiController]
+    [Route("api/deposito")]
+
     public class ControladorDeposito : ControllerBase
     {
+        private readonly IDepositoServicio depositoServicio;
+
+        public ControladorDeposito(IDepositoServicio depositoServicio)
+        {
+            this.depositoServicio = depositoServicio;
+        }
+        [HttpGet("obtener-depositos")]
+        public async Task<ActionResult<List<VerDepositoDTO>>> ObtenerDepositos()
+        {
+            var depositos= await depositoServicio.ObtenerDepositos();
+
+        }
+        [HttpGet("obtener-depositos/{id}")]
+        public async Task<ActionResult<VerDepositoDTO>> ObtenerDepositoPorId([FromRoute] int id)
+        {
+            ValueTuple<bool, VerDepositoDTO> res = await depositoServicio.ObtenerDepositoPorId(id);
+            if (res.Item1 && res.Item2 != null) return StatusCode(200, res.Item2);
+            else if (res.Item1 && res.Item2 == null) return StatusCode(404, "No existe un depósito con ese ID.");
+            else return StatusCode(500, res.Item2);
+        }
+        [HttpPost("crear")]
+        public async Task<ActionResult<string>> CrearDeposito([FromBody] DepositoAsociarDTO e)
+        {
+            ValueTuple<bool, string> res = await depositoServicio.CrearDeposito(e);
+            if (res.Item1) return StatusCode(201, res.Item2);
+            else if (res.Item2.Contains("ya existe")) return StatusCode(409, res.Item2);
+            else return StatusCode(500, res.Item2);
+        }
+
+        [HttpPut("actualizar/{id}")]
+        public async Task<ActionResult<string>> ActualizarDeposito([FromRoute] int id, [FromBody] DepositoAsociarDTO e)
+        {
+            if (id != e.Id) return StatusCode(400, "No se pudo realizar la operación");
+            ValueTuple<bool, string> res = await depositoServicio.ActualizarDeposito(e);
+            if (res.Item1) return StatusCode(200, res.Item2);
+            else if (res.Item2.Contains("ya existe")) return StatusCode(409, res.Item2);
+            else return StatusCode(500, res.Item2);
+        }
+        [HttpDelete("eliminar/{id}")]
+        public async Task<ActionResult<string>> EliminarDeposito([FromRoute] int id)
+        {
+            ValueTuple<bool, string> res = await depositoServicio.EliminarDeposito(id);
+            if (res.Item1) return StatusCode(200, res.Item2);
+            else if (res.Item2.Contains("No existe")) return StatusCode(404, res.Item2);
+            else return StatusCode(500, res.Item2);
+        }
     }
 }
---
+++ Repositorios/Servicios/DepositoServicio.cs
@@ -0,0 +1,20 @@
+﻿using Backend.Repositorios.Implementaciones;
+using Backend.DTO.DTOs_Depositos;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Backend.BD;
+
+namespace Backend.Repositorios.Servicios
+{
+    public class DepositoServicio : IDepositoServicio
+    {
+        public DepositoServicio(AppDbContext context, )
+        {
+
+        }
+    }
+}
