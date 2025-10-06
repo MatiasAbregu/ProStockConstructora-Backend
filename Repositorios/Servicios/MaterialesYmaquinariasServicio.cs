4d3ef4e2ce197ce88bb4229eb8e336fd57921b1f
master16mh <master16.mh@gmail.com>
2025-10-06 16:21:11 -0300

    cargarMyMenProceso

---
+++ Repositorios/Servicios/MaterialesYmaquinariasServicio.cs
@@ -0,0 +1,26 @@
+﻿using Backend.BD;
+using Backend.DTO.DTOs_MaterialesYmaquinarias;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backend.Repositorios.Servicios
+{
+    public class MaterialesYmaquinariasServicio
+    {
+        private readonly AppDbContext baseDeDatos;
+
+
+        public MaterialesYmaquinariasServicio(AppDbContext baseDeDatos)
+        {
+            this.baseDeDatos = baseDeDatos;
+        }
+
+        public Task<(bool, string)> MaterialYmaquinaCargarDTO(MaterialYmaquinaCargarDTO materialYmaquinaDTO)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
