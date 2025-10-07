585fabbd2ee655d96cebc8a8a357ff3ee06e2d6c
master16mh <master16.mh@gmail.com>
2025-10-07 16:10:24 -0300

    arreglando...

--- Repositorios/Implementaciones/IObraServicio.cs
+++ Repositorios/Implementaciones/IObraServicio.cs
@@ -2,10 +2,6 @@ using Backend.BD.Enums;
 using Backend.BD.Modelos;
 using Backend.DTO.DTOs_Obras;
 using Backend.DTOs;
-<<<<<<< HEAD
-using Backend.BD.Enums;
-=======
->>>>>>> cb2031fac0d027db9c0ad9023fca543014d2d4ef
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -17,21 +13,11 @@ namespace Backend.Repositorios.Implementaciones
     public interface IObraServicio
     {
         Task<(bool, string)> ActualizarEstadoObra(int id, EnumEstadoObra nuevoEstado);
-<<<<<<< HEAD
-        Task<(bool, string)> ActualizarObra(int id, Obra o);
-=======
         Task<(bool, string)> ActualizarObra(int id, ObraActualizarDTO o);
->>>>>>> cb2031fac0d027db9c0ad9023fca543014d2d4ef
         Task<(bool, string)> CrearObra(ObraAsociarDTO obraDTO);
         Task<(bool, VerObraDTO)> ObtenerObraPorId(int id);
         Task<(bool, List<VerObraDTO>)> ObtenerObras(int EmpresaId);
         Task<(bool, List<VerObraConDepositoDTO>)> ObtenerObrasConDeposito(int EmpresaId);
-<<<<<<< HEAD
-
-
-
-=======
->>>>>>> cb2031fac0d027db9c0ad9023fca543014d2d4ef
     }
 }
 
--- Repositorios/Servicios/ObraServicio.cs
+++ Repositorios/Servicios/ObraServicio.cs
@@ -17,30 +17,8 @@ using System.Threading.Tasks;
 
 namespace Backend.Repositorios.Servicios
 {
-    public class ObraServicio : IObraServicio {
-
-
-    private readonly AppDbContext baseDeDatos;
-    private readonly IDepositoServicio depositoServicio;
-
-
-    public ObraServicio(AppDbContext baseDeDatos)
+    public class ObraServicio : IObraServicio
     {
-<<<<<<< HEAD
-        this.baseDeDatos = baseDeDatos;
-    }
-
-    public async Task<(bool, List<VerObraDTO>)> ObtenerObras(int EmpresaId)
-    {
-        try
-        {
-            List<VerObraDTO> obrasVer = [];
-            List<Obra> obras = await baseDeDatos.Obras.Where(o => o.EmpresaId == EmpresaId).ToListAsync();
-
-            foreach (Obra o in obras)
-            {
-                obrasVer.Add(new VerObraDTO
-=======
 
         private readonly AppDbContext baseDeDatos;
         private readonly IDepositoServicio depositoServicio;
@@ -115,14 +93,10 @@ namespace Backend.Repositorios.Servicios
                 Obra o = await baseDeDatos.Obras.FirstOrDefaultAsync(o => o.Id == id);
                 if (o == null) return (true, null);
                 VerObraDTO obraVer = new VerObraDTO
->>>>>>> cb2031fac0d027db9c0ad9023fca543014d2d4ef
                 {
                     EmpresaId = o.EmpresaId,
                     Nombre = o.NombreObra,
                     Estado = o.Estado.ToString()
-<<<<<<< HEAD
-                });
-=======
                 };
                 return (true, obraVer);
             }
@@ -138,7 +112,7 @@ namespace Backend.Repositorios.Servicios
             try
             {
                 bool existeObra = await baseDeDatos.Obras
-                    .AnyAsync(ob => obraDTO.NombreObra.ToLower() == ob.NombreObra.ToLower() 
+                    .AnyAsync(ob => obraDTO.NombreObra.ToLower() == ob.NombreObra.ToLower()
                     && ob.EmpresaId == obraDTO.EmpresaId);
                 if (existeObra)
                     return (false, "Ya existe una obra con ese nombre en la empresa.");
@@ -167,8 +141,6 @@ namespace Backend.Repositorios.Servicios
                 Obra obraUpdate = await baseDeDatos.Obras.FirstOrDefaultAsync(ob => ob.Id == id);
                 if (obraUpdate == null) return (false, "No existe una obra con ese ID.");
                 obraUpdate.NombreObra = o.NombreObra;
-
-
                 await baseDeDatos.SaveChangesAsync();
                 return (true, "Obra actualizada con éxito.");
             }
@@ -193,282 +165,7 @@ namespace Backend.Repositorios.Servicios
             {
                 Console.WriteLine($"Error: {ex.InnerException.Message}");
                 return (false, "Error al actualizar el estado de la obra.");
->>>>>>> cb2031fac0d027db9c0ad9023fca543014d2d4ef
             }
-
-            return (true, obrasVer);
         }
-        catch (Exception ex)
-        {
-            Console.WriteLine($"Error: {ex.InnerException.Message}");
-            return (false, null);
-        }
-    }
-
-    public async Task<(bool, List<VerObraConDepositoDTO>)> ObtenerObrasConDeposito(int EmpresaId)
-    {
-        try
-        {
-            List<VerObraConDepositoDTO> obrasVer = [];
-            var obrasConDepositos = await (from o in baseDeDatos.Obras
-                                           join d in baseDeDatos.Depositos on o.Id equals d.ObraId
-                                           where o.EmpresaId == EmpresaId
-                                           select new { Obra = o, Deposito = d }).ToListAsync();
-            foreach (var item in obrasConDepositos)
-            {
-                obrasVer.Add(new VerObraConDepositoDTO
-                {
-                    Id = item.Obra.Id,
-                    Nombre = item.Obra.NombreObra,
-                    Estado = item.Obra.Estado.ToString(),
-                    DepositoId = item.Deposito.Id,
-                    TipoDeposito = item.Deposito.TipoDeposito.ToString()
-                });
-            }
-            return (true, obrasVer);
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine($"Error: {ex.InnerException.Message}");
-            return (false, null);
-        }
-    }
-
-    public async Task<(bool, VerObraDTO)> ObtenerObraPorId(int id)
-    {
-        try
-        {
-            Obra o = await baseDeDatos.Obras.FirstOrDefaultAsync(o => o.Id == id);
-            if (o == null) return (true, null);
-            VerObraDTO obraVer = new VerObraDTO
-            {
-                Id = o.Id,
-                Nombre = o.NombreObra,
-                Estado = o.Estado.ToString()
-            };
-            return (true, obraVer);
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine($"Error: {ex.InnerException.Message}");
-            return (false, null);
-        }
-    }
-
-    public async Task<(bool, string)> CrearObra(ObraAsociarDTO obraDTO)
-    {
-        try
-        {
-            bool existeObra = await baseDeDatos.Obras.AnyAsync(ob => obraDTO.NombreObra.ToLower() == obraDTO.NombreObra.ToLower() && ob.EmpresaId == obraDTO.EmpresaId);
-            if (existeObra)
-                return (false, "Ya existe una obra con ese nombre en la empresa.");
-
-            var nuevaObra = new Obra
-            {
-                NombreObra = obraDTO.NombreObra,
-                EmpresaId = obraDTO.EmpresaId,
-                Estado = EnumEstadoObra.EnProceso
-            };
-
-            await baseDeDatos.Obras.AddAsync(nuevaObra);
-            await baseDeDatos.SaveChangesAsync();
-            return (true, "Obra creada con éxito.");
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine($"Error: {ex.InnerException.Message}");
-            return (false, "Error al crear la obra.");
-        }
-    }
-
-    public async Task<(bool, string)> ActualizarObra(int id, Obra o)
-    {
-        try
-        {
-            Obra obraUpdate = await baseDeDatos.Obras.FirstOrDefaultAsync(ob => ob.Id == id);
-            if (obraUpdate == null) return (false, "No existe una obra con ese ID.");
-            obraUpdate.NombreObra = o.NombreObra;
-            obraUpdate.Estado = o.Estado;
-
-            await baseDeDatos.SaveChangesAsync();
-            return (true, "Obra actualizada con éxito.");
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine($"Error: {ex.InnerException.Message}");
-            return (false, "Error al actualizar la obra.");
-        }
-    }
-
-    public async Task<(bool, string)> ActualizarEstadoObra(int id, EnumEstadoObra nuevoEstado)
-    {
-        try
-        {
-            Obra obra = await baseDeDatos.Obras.FirstOrDefaultAsync(o => o.Id == id);
-            if (obra == null) return (false, "No existe una obra con ese ID.");
-            obra.Estado = nuevoEstado;
-            await baseDeDatos.SaveChangesAsync();
-            return (true, "Estado de la obra actualizado con éxito.");
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine($"Error: {ex.InnerException.Message}");
-            return (false, "Error al actualizar el estado de la obra.");
-        }
-    }
-<<<<<<< HEAD
-}
-
-}
-
-private readonly AppDbContext baseDeDatos;
-private readonly IDepositoServicio depositoServicio;
-
-
-public ObraServicio(AppDbContext baseDeDatos)
-{
-    this.baseDeDatos = baseDeDatos;
-}
-
-public async Task<(bool, List<VerObraDTO>)> ObtenerObras(int EmpresaId)
-{
-    try
-    {
-        List<VerObraDTO> obrasVer = [];
-        List<Obra> obras = await baseDeDatos.Obras.Where(o => o.EmpresaId == EmpresaId).ToListAsync();
-
-        foreach (Obra o in obras)
-        {
-            obrasVer.Add(new VerObraDTO
-            {
-                EmpresaId = o.EmpresaId,
-                Nombre = o.NombreObra,
-                Estado = o.Estado.ToString()
-            });
-        }
-
-        return (true, obrasVer);
-    }
-    catch (Exception ex)
-    {
-        Console.WriteLine($"Error: {ex.InnerException.Message}");
-        return (false, null);
     }
 }
-
-public async Task<(bool, List<VerObraConDepositoDTO>)> ObtenerObrasConDeposito(int EmpresaId)
-{
-    try
-    {
-        List<VerObraConDepositoDTO> obrasVer = [];
-        var obrasConDepositos = await (from o in baseDeDatos.Obras
-                                       join d in baseDeDatos.Depositos on o.Id equals d.ObraId
-                                       where o.EmpresaId == EmpresaId
-                                       select new { Obra = o, Deposito = d }).ToListAsync();
-        foreach (var item in obrasConDepositos)
-        {
-            obrasVer.Add(new VerObraConDepositoDTO
-            {
-                Id = item.Obra.Id,
-                Nombre = item.Obra.NombreObra,
-                Estado = item.Obra.Estado.ToString(),
-                DepositoId = item.Deposito.Id,
-                TipoDeposito = item.Deposito.TipoDeposito.ToString()
-            });
-        }
-        return (true, obrasVer);
-    }
-    catch (Exception ex)
-    {
-        Console.WriteLine($"Error: {ex.InnerException.Message}");
-        return (false, null);
-    }
-}
-
-public async Task<(bool, VerObraDTO)> ObtenerObraPorId(int id)
-{
-    try
-    {
-        Obra o = await baseDeDatos.Obras.FirstOrDefaultAsync(o => o.Id == id);
-        if (o == null) return (true, null);
-        VerObraDTO obraVer = new VerObraDTO
-        {
-            Id = o.Id,
-            Nombre = o.NombreObra,
-            Estado = o.Estado.ToString()
-        };
-        return (true, obraVer);
-    }
-    catch (Exception ex)
-    {
-        Console.WriteLine($"Error: {ex.InnerException.Message}");
-        return (false, null);
-    }
-}
-
-public async Task<(bool, string)> CrearObra(ObraAsociarDTO obraDTO)
-{
-    try
-    {
-        bool existeObra = await baseDeDatos.Obras.AnyAsync(ob => obraDTO.NombreObra.ToLower() == obraDTO.NombreObra.ToLower() && ob.EmpresaId == obraDTO.EmpresaId);
-        if (existeObra)
-            return (false, "Ya existe una obra con ese nombre en la empresa.");
-
-        var nuevaObra = new Obra
-        {
-            NombreObra = obraDTO.NombreObra,
-            EmpresaId = obraDTO.EmpresaId,
-            Estado = EnumEstadoObra.EnProceso
-        };
-
-        await baseDeDatos.Obras.AddAsync(nuevaObra);
-        await baseDeDatos.SaveChangesAsync();
-        return (true, "Obra creada con éxito.");
-    }
-    catch (Exception ex)
-    {
-        Console.WriteLine($"Error: {ex.InnerException.Message}");
-        return (false, "Error al crear la obra.");
-    }
-}
-
-public async Task<(bool, string)> ActualizarObra(int id, Obra o)
-{
-    try
-    {
-        Obra obraUpdate = await baseDeDatos.Obras.FirstOrDefaultAsync(ob => ob.Id == id);
-        if (obraUpdate == null) return (false, "No existe una obra con ese ID.");
-        obraUpdate.NombreObra = o.NombreObra;
-        obraUpdate.Estado = o.Estado;
-
-        await baseDeDatos.SaveChangesAsync();
-        return (true, "Obra actualizada con éxito.");
-    }
-    catch (Exception ex)
-    {
-        Console.WriteLine($"Error: {ex.InnerException.Message}");
-        return (false, "Error al actualizar la obra.");
-    }
-}
-
-public async Task<(bool, string)> ActualizarEstadoObra(int id, EnumEstadoObra nuevoEstado)
-{
-    try
-    {
-        Obra obra = await baseDeDatos.Obras.FirstOrDefaultAsync(o => o.Id == id);
-        if (obra == null) return (false, "No existe una obra con ese ID.");
-        obra.Estado = nuevoEstado;
-        await baseDeDatos.SaveChangesAsync();
-        return (true, "Estado de la obra actualizado con éxito.");
-    }
-    catch (Exception ex)
-    {
-        Console.WriteLine($"Error: {ex.InnerException.Message}");
-        return (false, "Error al actualizar el estado de la obra.");
-    }
-}
-
-    
-=======
-}
->>>>>>> cb2031fac0d027db9c0ad9023fca543014d2d4ef
