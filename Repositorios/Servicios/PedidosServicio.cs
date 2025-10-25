7b26e6eca1c1431e3cf5a9a57c8307bc76669c64
master16mh <master16.mh@gmail.com>
2025-10-25 17:20:09 -0300

    subiendo ultimos changes

---
+++ Repositorios/Servicios/PedidosServicio.cs
@@ -0,0 +1,26 @@
+﻿using Backend.BD;
+using Backend.Repositorios.Implementaciones;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backend.Repositorios.Servicios
+{
+    public class PedidosServicio : IPedidosServicio
+    {
+        private readonly AppDbContext baseDeDatos;
+        private readonly IPedidosServicio pedidosServicio;
+
+        public PedidosServicio(AppDbContext baseDeDatos)
+        {
+            this.baseDeDatos = baseDeDatos;
+        }
+
+        public async Task
+        {
+            
+        }
+    }
+}
