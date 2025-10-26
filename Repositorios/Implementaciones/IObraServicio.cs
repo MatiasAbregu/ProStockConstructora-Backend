30f8f5c2cbb437bcbdbd4754c0d622e4253a69f9
MatiasAbregu <matiasabregu506@gmail.com>
2025-10-26 17:35:58 -0300

    CORRIGIENDO AHORA UN ERROR MIO DIOS SANTO

--- Repositorios/Implementaciones/IObraServicio.cs
+++ Repositorios/Implementaciones/IObraServicio.cs
@@ -13,7 +13,6 @@ namespace Backend.Repositorios.Implementaciones
     public interface IObraServicio
     {
         Task<(bool, string)> CrearObra(CrearObraDTO obraDTO);
-        Task<(bool, string)> ActualizarEstadoObra(int id, EnumEstadoObra nuevoEstado);
         Task<(bool, string)> ActualizarObra(int id, ObraActualizarDTO o);
         Task<(bool, VerObraDTO)> ObtenerObraPorId(int id);
         Task<(bool, List<VerObraDTO>)> ObtenerObras(int EmpresaId);
