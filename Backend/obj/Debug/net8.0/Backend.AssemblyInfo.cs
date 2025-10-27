216da710a9015df86c514b612c3bc79281ac2233
master16mh <master16.mh@gmail.com>
2025-10-27 01:39:05 -0300

    nombre: cemento - tipo: MAQUINA

--- Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f961f157a6375cf411bc518b03a035101c34aea")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a07c5729e45d1b90ad06ed6588c01d760c8136e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
