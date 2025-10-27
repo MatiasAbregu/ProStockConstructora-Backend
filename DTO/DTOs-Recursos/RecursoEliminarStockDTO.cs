a57b338faafbb8565f02edf9b46e93c1303da39f
master16mh <master16.mh@gmail.com>
2025-10-27 17:01:40 -0300

    eliminado datos al p del dto

--- DTO/DTOs-Recursos/RecursoEliminarStockDTO.cs
+++ DTO/DTOs-Recursos/RecursoEliminarStockDTO.cs
@@ -10,12 +10,5 @@ namespace Backend.DTO.DTOs_Recursos
     public class RecursoEliminarStockDTO
     {
         public int StockId { get; set; }
-        public string CodigoISO { get; set; }
-        public string Nombre { get; set; }
-        public EnumTipoMaterialoMaquina Tipo { get; set; }
-        public string? TipoMaterial { get; set; }
-        public string? UnidadMedida { get; set; }
-        public string Descripcion { get; set; }
-        public int Cantidad { get; set; }
     }
 }
