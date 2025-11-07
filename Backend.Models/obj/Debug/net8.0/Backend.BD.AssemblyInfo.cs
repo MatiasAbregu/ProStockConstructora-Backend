65e7da7e9db8fadc28020a1cc56a52c95cf99173
master16mh <master16.mh@gmail.com>
2025-11-07 00:27:12 -0300

    controller uniMedida y arreglo en teoria de recursos

--- Backend.Models/obj/Debug/net8.0/Backend.BD.AssemblyInfo.cs
+++ Backend.Models/obj/Debug/net8.0/Backend.BD.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend.BD")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7808bd7f3b9bdb214c2d32f143e7ffaa3380e080")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+11e7420f64774ef68de8fb89cbf3c8e7642e77be")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend.BD")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend.BD")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
